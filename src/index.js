ee64366a7380a42a00c77a09baadaeea1480eada
kristenmalani <kmalani.valle@gmail.com>
2021-07-04 20:49:26 -0700

    moved project into repository (per lesson 4 challenge)

---
+++ src/index.js
@@ -0,0 +1,101 @@
+// format for date on weather app
+// Wednesday Jun 30, 2021 as of 4:44 PM
+
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h2.innerHTML = `${day} ${month} ${date}, ${year} at ${hours}:${minutes}`;
+
+// add search engine, when searching for city, display city name after user submits form
+// On your project, when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#currentTemp");
+  currentTemperature.innerHTML = `${temperature}`;
+  let description = document.querySelector("#temperature-description");
+  description.innerHTML = response.data.weather[0].description;
+}
+
+function searching(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+  let h1 = document.querySelector("h1");
+  if (searchInput.value) {
+    h1.innerHTML = `${searchInput.value}`;
+  } else {
+    h1.innerHTML = null;
+    alert("Please type a city");
+  }
+  let city = `${searchInput.value}`;
+  let apiKey = "a0fc8949603a1172276df866d06c037a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searching);
+
+// current location button
+
+function showCurrent(response) {
+  console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#currentTemp");
+  currentTemperature.innerHTML = `${temperature}`;
+  let city = response.data.name;
+  let currentCity = document.querySelector("#city");
+  currentCity.innerHTML = `${city}`;
+  let description = document.querySelector("#temperature-description");
+  description.innerHTML = response.data.weather[0].description;
+}
+
+function retrievePosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "imperial";
+  let apiKey = "a0fc8949603a1172276df866d06c037a";
+  let apiEndpoint = "http://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showCurrent);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let button = document.querySelector("#current-location");
+button.addEventListener("click", getCurrentPosition);
