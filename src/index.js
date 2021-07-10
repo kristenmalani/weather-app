73a0303737dc8a1e3d3b1fff3991f2a24f86333b
kristenmalani <kmalani.valle@gmail.com>
2021-07-09 21:54:28 -0700

    added search engine with button & temperature unit conversion function, re-organized temperature div/spans

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ let now = new Date();
 let h2 = document.querySelector("h2");
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let year = now.getFullYear();
 
 let days = [
@@ -42,38 +48,30 @@ h2.innerHTML = `${day} ${month} ${date}, ${year} | ${hours}:${minutes}`;
 // On your project, when a user searches for a city (example: New York), it should display the name of the city on the result page and the current temperature of the city.
 
 function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemperature = document.querySelector("#currentTemp");
-  currentTemperature.innerHTML = `${temperature}`;
-  let description = document.querySelector("#temperature-description");
-  description.innerHTML = response.data.weather[0].description;
-}
+  let temperatureElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#temperature-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#current-icon");
 
-function searching(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  let h1 = document.querySelector("h1");
-  if (searchInput.value) {
-    h1.innerHTML = `${searchInput.value}`;
-  } else {
-    h1.innerHTML = null;
-    alert("Please type a city");
-  }
-  let city = `${searchInput.value}`;
-  let apiKey = "a0fc8949603a1172276df866d06c037a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searching);
 
 // current location button
 
-function showCurrent(response) {
-  console.log(response.data);
+function displayTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
-  let currentTemperature = document.querySelector("#currentTemp");
+  let currentTemperature = document.querySelector("#current-temp");
   currentTemperature.innerHTML = `${temperature}`;
   let city = response.data.name;
   let currentCity = document.querySelector("#city");
@@ -85,12 +83,12 @@ function showCurrent(response) {
 function retrievePosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let units = "imperial";
+  let units = "metric";
   let apiKey = "a0fc8949603a1172276df866d06c037a";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
-  axios.get(apiUrl).then(showCurrent);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function getCurrentPosition() {
@@ -99,3 +97,48 @@ function getCurrentPosition() {
 
 let button = document.querySelector("#current-location");
 button.addEventListener("click", getCurrentPosition);
+
+// search engine & temperature unit conversion
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function search(city) {
+  let apiKey = "a0fc8949603a1172276df866d06c037a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temp");
+  // remove the active class of celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("San Diego");
