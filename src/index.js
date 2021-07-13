d91caf6f884be2064b01158f47a6320356921e2c
kristenmalani <kmalani.valle@gmail.com>
2021-07-12 20:33:19 -0700

    updated index of forecast

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 4) {
+    if (index < 5) {
       forecastHTML =
         forecastHTML +
         `
